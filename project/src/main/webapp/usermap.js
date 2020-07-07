f97ca455375bf50820c07e21157e111466f9ddf8
gccurtis <gccurtis@google.com>
2020-07-07 19:10:55 +0000

    for everyone

--- project/src/main/webapp/usermap.js
+++ project/src/main/webapp/usermap.js
@@ -1,4 +1,27 @@
-
 // get filters
 // max distance
 // use maps, applys to make userMap
+
+const apiKey = "AIzaSyDEVKOx6yO8pM10Jj39HXSkS4HYEv8nBJ8";
+
+function initMap() {
+  // Create the map.
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 52.632469, lng: -1.689423},
+  });
+
+  // Load the stores GeoJSON onto the map.
+  map.data.loadGeoJson('stores.json', {idPropertyName: 'storeid'});
+
+  // Define the custom marker icons, using the store's "category".
+  map.data.setStyle((feature) => {
+    return {
+      icon: {
+        url: `img/icon_${feature.getProperty('category')}.png`,
+        scaledSize: new google.maps.Size(64, 64),
+      },
+    };
+  });
+}
+
