8f69b4ffe9c786e5477cc6d6da20a5f16eea6c91
gccurtis <gccurtis@google.com>
2020-07-10 14:59:25 +0000

    Made a function for only displaying searched places, and displays markers

--- project/src/main/webapp/usermap.js
+++ project/src/main/webapp/usermap.js
@@ -6,8 +6,22 @@
 const apiKey = "AIzaSyDEVKOx6yO8pM10Jj39HXSkS4HYEv8nBJ8";
 var currentZoom = 6;
 var currentRadius = 5000;
-var nearbyPlaces = [];
 var currentLocation;
+var nearbyMarkerList = [];
+var searchedMarkerList = [];
+
+//List of Places Objects for other processing
+//var nearbyPlaces = [];
+
+//List of IDs for Tamajong
+//var usermap = [];
+
+//Promise for a list of nearby places
+var nearbyPlacesPromise;
+
+
+//Promise for a list of searched places
+var searchedPlacesPromise;
 
 var map = new google.maps.Map(document.getElementById('map'), {
 	center: {lat: 0, lng: 0},
@@ -18,7 +32,8 @@ async function initMap() {
 	currentLocation = await getCurrentLocation();
 	map.setCenter(currentLocation);
 	displayMap(map,currentZoom);
-	setNearbyPlaces(map, currentLocation, currentRadius, 'resturant');
+	nearbyPlacesPromise = getSearchedPlaces(map, currentRadius, 'restaurant');
+	nearbyPlacesPromise.then((li) => li.forEach((x) => addMarker(nearbyMarkerList, map, x.geometry.location, x.name, x.icon)));
 }
 
 function getCurrentLocation() {
@@ -47,22 +62,55 @@ function displayMap(map ,zoom) {
 	infoWindow.open(map);
 }
 
-function setNearbyPlaces(map, clocation, radius, type) {
+function getNearbyPlaces(map, radius, type) {
 	request = {
-		location: clocation,
+		location: map.center,
 		radius: String(radius),
 		type: [type]
 	};
 	service = new google.maps.places.PlacesService(map);
-	service.nearbySearch(request, placesCallback);
+	return new Promise((resolve, reject) => service.nearbySearch(request, (results, pstatus) => resolve(placesCallback(results, pstatus))));
+}
+
+function getSearchedPlaces(map, radius, query) {
+	request = {
+		location: map.center,
+		radius: String(radius),
+		query: query
+	};
+	service = new google.maps.places.PlacesService(map);
+	return new Promise((resolve, reject) => service.textSearch(request, (results, pstatus) => resolve(placesCallback(results, pstatus))));
 }
 
 function placesCallback(results, pstatus) {
+	var placesList = [];
 	if (pstatus == google.maps.places.PlacesServiceStatus.OK) {
 		for(let i = 0; i < results.length; i++){
-			nearbyPlaces.push(results[i]);
+			placesList.push(results[i]);
 		}
 	}
+	return placesList;
 }
 
+
+function addMarker(markerList, map, location, labelText, imageLink) {
+  var marker = new google.maps.Marker({
+    position: location,
+    label: labelText,
+    icon: imageLink,
+    map: map
+  });
+  markerList.push(marker);
+}
+
+function displaySearchResults() {
+	searchedPlacesPromise = getSearchedPlaces(map, currentRadius, document.getElementById("searchbox").value);
+	nearbyMarkerList.forEach((x) => x.setMap(null));
+	searchedPlacesPromise.then((li) => li.forEach((x) => addMarker(searchedMarkerList, map, x.geometry.location, x.name, x.icon)));
+}
+
+
 initMap();
+
+
+
